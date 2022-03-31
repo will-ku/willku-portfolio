344c2a69f206708e49523024c7036f1176e846fd
William Ku <william.ku9192@gmail.com>
2022-03-30 21:29:09 -0400

    preload images

--- src/App.js
+++ src/App.js
@@ -3,6 +3,17 @@ import Routes from "./router/Routes"
 
 
 const App = () => {
+  const aboutHeadshot = new Image()
+  aboutHeadshot.src = "https://pixsy-dev.s3.us-east-2.amazonaws.com/port/aboutmedarkshorter.jpg"
+
+  const pixsyThumbnail = new Image()
+  pixsyThumbnail.src = "https://pixsy-dev.s3.us-east-2.amazonaws.com/port/portpixsy.png"
+
+  const homeCourtThumbnail = new Image()
+  homeCourtThumbnail.src = "https://pixsy-dev.s3.us-east-2.amazonaws.com/port/porthomecourt.png"
+
+  const nycVizThumbnail = new Image()
+  nycVizThumbnail.src = "https://pixsy-dev.s3.us-east-2.amazonaws.com/port/portnycviz.png"
 
   return (
     <div className="tokyo_tm_all_wrap">  
